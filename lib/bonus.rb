0cfcf55c33fd566784efc3b0ce95a7080c647b46
Tegan Sims <tegansims@hotmail.com>
2019-07-24 13:11:33 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -28,3 +28,5 @@ def bonus
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
+
+p epic_tragedy[:heroine][:heroine_friends][:age]
